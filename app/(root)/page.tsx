e7d451f090ba1562c64c54198e39cd79b2a5dc70
DHRUV-EDDI <dhruv2018570@gmail.com>
2024-03-17 21:59:25 +0530

    Project setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      // HERO SECTION
+
+      // Testinomals 
+      
+      // Products
+
+      // FAQ
+    </>
+  );
+}
