4f09a6e4ba6ff841e77a90121f50d6d31ff97c5d
bharadwaj <kurashiva11@gmail.com>
2024-04-10 18:41:06 +0530

    fix static image path

--- components/shared/StickyContactItems.tsx
+++ components/shared/StickyContactItems.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
+import { normalizedPath } from "../../lib/utils";
 
 export default function StickyContactItems() {
     return (
         <div className="fixed right-0 top-1/3">
-          <span onClick={() => window.open('mailto:kurashiva11@gmail.com', '_blank')}><Image src={"/gmail.png"} alt="gmail" width={48} height={48} /></span>
+          <span onClick={() => window.open('mailto:kurashiva11@gmail.com', '_blank')}><Image src={normalizedPath("/gmail.png")} alt="gmail" width={48} height={48} /></span>
           <div className="h-1" />
-          <span onClick={() => window.open('tel:8096243936', '_blank')}><Image src={"/telephone.png"} alt="phone" width={39} height={39} /></span>
+          <span onClick={() => window.open('tel:8096243936', '_blank')}><Image src={normalizedPath("/telephone.png")} alt="phone" width={39} height={39} /></span>
           <div className="h-1" />
-          <span onClick={() => window.open('https://api.whatsapp.com/send?phone=8096243936', '_blank')}><Image src={"/whatsapp.svg"} alt="whatsapp" width={48} height={48} /></span>
+          <span onClick={() => window.open('https://api.whatsapp.com/send?phone=8096243936', '_blank')}><Image src={normalizedPath("/whatsapp.svg")} alt="whatsapp" width={48} height={48} /></span>
         </div>
     );
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -7,7 +7,7 @@ export function cn(...inputs: ClassValue[]) {
 
 
 export function normalizedPath(assetPath: string) {
-  const config = require("../next.config.mjs");
+  const config = require("../next.config.js");
   const path = require("path");
   return config.basePath?.length > 0 ? path.join(config.basePath, assetPath) : assetPath;
 }
